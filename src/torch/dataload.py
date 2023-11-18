f0a8a384c59a7c2bfbafe7fbb8e2505a934a7d68
Daniil Dits <daniil.dits@gmail.com>
2023-11-18 13:48:45 +0300

    add method read from file

--- src/torch/dataload.py
+++ src/torch/dataload.py
@@ -1,74 +1,19 @@
 import pandas as pd
-import os
 from torch.utils.data import DataLoader, random_split, Dataset, TensorDataset
 import torch
 
 
-def load_data(path="biaxial_three_different_holes.xlsx", validation=False,
-              split=True, extended_data=False):
-    if extended_data:
-
-        files = os.listdir(os.path.join("..", "data"))[:5]
-        # print(files)
-        df_list = []
-        for file in files:
-            df_list.append(pd.read_excel(os.path.join('data', file)))
-
-        df = pd.concat(df_list)
-
-    else:
-
-        file = os.path.join('data', path)
-        df = pd.read_excel(file)
-
-    # n = df.shape[0]
-    # print("DataFrame shape =", n)
-
-    X = df.drop(columns=['d(psi)/d(I1)', 'd(psi)/d(I2)'])
-    y = df.drop(columns=['d(psi)/d(I1)', 'I1', 'I2'])
-
-    # print("X shape = ", X.shape)
-    # print("y shape = ", y.shape)
-    # print(X["I1"].values)
-
-    # y = df.drop(columns=['I1', 'I2'])
-
-    if not split:
-        return X, y
-
-    X_train, X_test, y_train, y_test = \
-        random_split(X, y,
-                     test_size=0.2, random_state=2)
-
-    if not validation:
-        return X_train, X_test, y_train, y_test
-
-    X_train, X_val, y_train, y_val = \
-        train_test_split(X_train, y_train,
-                         test_size=0.25, random_state=2)
-
-    print("train")
-    print("X shape = ", X_train.shape)
-    print("y shape = ", y_train.shape)
-    print("Test")
-    print("X shape = ", X_test.shape)
-    print("y shape = ", y_test.shape)
-    print("Validation")
-    print("X shape = ", X_val.shape)
-    print("y shape = ", y_val.shape)
-
-    return X_train, X_test, X_val, y_train, y_test, y_val
-
 class ExcelDataset(Dataset):
-    def __init__(self, excel_files, transform=None):
+    def __init__(self, path="full_data_names.txt", transform=None):
 
-        data_frames = [pd.read_excel(file) for file in excel_files]
-        data = pd.concat(data_frames, ignore_index=True)
-        self.data = data
+        self.path = path
 
-        self.features = torch.tensor(data.drop(columns=['d(psi)/d(I1)', 'd(psi)/d(I2)']).values,
+        if path is not None:
+            self.data = self.read_from_file()
+
+        self.features = torch.tensor(self.data.drop(columns=['d(psi)/d(I1)', 'd(psi)/d(I2)']).values,
                                      dtype=torch.float32)
-        self.target = torch.tensor(data.drop(columns=['d(psi)/d(I1)', 'I1', 'I2']).values,
+        self.target = torch.tensor(self.data.drop(columns=['d(psi)/d(I1)', 'I1', 'I2']).values,
                                    dtype=torch.float32)
         self.transform = transform
 
@@ -84,6 +29,18 @@ class ExcelDataset(Dataset):
 
         return features, target
 
+    # def __str__(self, ):
+    def read_from_file(self):
+        excel_files = []
+
+        with open(self.path, "r") as file:
+            for table_name in file:
+                excel_files.append(table_name[:-1])
+
+        data_frames = [pd.read_excel(file) for file in excel_files]
+
+        return pd.concat(data_frames, ignore_index=True)
+
 
 if __name__ == "__main__":
 
@@ -92,24 +49,35 @@ if __name__ == "__main__":
         std = data.std()
         return (data - mean) / std
 
-    file_path = "../../data"
-    excel_files = os.listdir(file_path)
-    excel_files = [os.path.join(file_path, file) for file in excel_files][:2]
 
-    dataset = ExcelDataset(excel_files)
+    # file_path = "../../data"
+    # excel_files = os.listdir(file_path)
+    # excel_files = [os.path.join(file_path, file) for file in excel_files][:2]
+
+    # excel_files = []
+    # f = open(r"full_data_names.txt")
+    # for lines in f:
+    #     excel_files.append(lines[:-1])
+    dataset = ExcelDataset("one_data_names.txt.txt")
 
     f = dataset.features
     t = dataset.target
     print(dataset.features)
+    print("фичи: \n", f)
+
     f = normalize_data(f)
-    print(f)
-    features_tensor = torch.tensor(f.values, dtype=torch.float32)
-    target_tensor = torch.tensor(t.values, dtype=torch.float32)
-    print(type(features_tensor), type(target_tensor))
-    # d = Dataset()
-    dataset = TensorDataset(features_tensor, target_tensor)
+    t = normalize_data(t)
+    print("нормализованные фичи: \n", f)
+    print("тип фичей -", type(f))
+
+    dataset = TensorDataset(f, t)
     data_loader = DataLoader(dataset, batch_size=64, shuffle=False)
 
+    train_size = int(0.9 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
 
-
-
+    print("размер трейна =", len(train_dataset))
+    print("размер теста =", len(test_dataset))
+    print("трейн: \n", train_dataset[10:20])
+    print("тест: \n",test_dataset[10:20])
