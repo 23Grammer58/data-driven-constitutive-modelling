192d6e002158bd446c4448ce9b2d08db5cc199dc
Daniil Dits <daniil.dits@gmail.com>
2024-02-03 14:58:24 +0300

    add normalization data

--- src/torch/dataload.py
+++ src/torch/dataload.py
@@ -19,6 +19,12 @@ def NeoHookean_psi(I1, I2):
     return 6600 * (I1 + 1/I2 - 3)
 
 
+def normalize_data(data):
+    mean = data.mean()
+    std = data.std()
+    return (data - mean) / std
+
+
 class ExcelDataset(Dataset):
     def __init__(self, path="full_data_names.txt", transform=None):
         super(Dataset, self).__init__()
@@ -35,7 +41,11 @@ class ExcelDataset(Dataset):
         # self.target = torch.tensor(NeoHookean_psi(self.features['I1'], self.features['I2']))
         # self.target = self.data[""]
         self.target = torch.tensor(self.data.apply(
-            lambda row: NeoHookean_psi(row['I1'], row['I2']), axis=1).values, dtype=torch.float32)
+            lambda row: NeoHookean_psi(row['I1'], row['I2']), axis=1).values, dtype=torch.float32).unsqueeze(-1)
+
+        if transform is not None:
+            self.features = normalize_data(self.features)
+            self.target = normalize_data(self.target)
         self.transform = transform
 
     def __len__(self):
@@ -81,21 +91,21 @@ if __name__ == "__main__":
         return (data - mean) / std
 
 
-    data = list(range(100))
-    dataset = SimpleDataset(data)
-    # Задаем размеры для каждой части (train_size, test_size)
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-
-    # Используем random_split для разделения dataset
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-
-    # Выводим размеры полученных наборов данных
-    print(f"Размер обучающей выборки: {len(train_dataset)}")
-    print(f"Размер тестовой выборки: {len(test_dataset)}")
-    for idx in range(len(test_dataset)):
-        data_point = test_dataset[idx]
-        print(f"Элемент {idx + 1}: {data_point}")    # file_path = "../../data"
+    # data = list(range(100))
+    # dataset = SimpleDataset(data)
+    # # Задаем размеры для каждой части (train_size, test_size)
+    # train_size = int(0.8 * len(dataset))
+    # test_size = len(dataset) - train_size
+    #
+    # # Используем random_split для разделения dataset
+    # train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    #
+    # # Выводим размеры полученных наборов данных
+    # print(f"Размер обучающей выборки: {len(train_dataset)}")
+    # print(f"Размер тестовой выборки: {len(test_dataset)}")
+    # for idx in range(len(test_dataset)):
+    #     data_point = test_dataset[idx]
+    #     print(f"Элемент {idx + 1}: {data_point}")    # file_path = "../../data"
     # excel_files = os.listdir(file_path)
     # excel_files = [os.path.join(file_path, file) for file in excel_files][:2]
 
@@ -103,28 +113,36 @@ if __name__ == "__main__":
     # f = open(r"full_data_names.txt")
     # for lines in f:
     #     excel_files.append(lines[:-1])
-    # dataset = ExcelDataset("one_data_names.txt")
+    dataset = ExcelDataset("one_data_names.txt")
     #
-    # f = dataset.features
-    # t = dataset.target
-    # print(dataset.features)
-    # print("фичи: \n", f)
-    #
+    f = dataset.features
+    t = dataset.target
+    print(dataset.features)
+    print("фичи: \n", f)
+
     # f = normalize_data(f)
     # t = normalize_data(t)
-    # print("нормализованные фичи: \n", f)
-    # print("тип фичей -", type(f))
-    #
-    # dataset = TensorDataset(f, t)
-    # train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False, pin_memory=True)
-    #
+    print("нормализованные фичи: \n", f)
+    print("тип фичей -", type(f))
+    print("тип target -", type(t))
+
+    # t = torch.stack(list(map(lambda x: torch.unsqueeze(x, 0), t)))
+    print(t)
+
+    dataset = TensorDataset(f, t)
+
+
     # train_size = int(0.9 * len(dataset))
     # test_size = len(dataset) - train_size
     # train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-    #
+    # #
     # print("размер трейна =", len(train_dataset))
     # print("размер теста =", len(test_dataset))
-    # print("трейн: \n", train_dataset[10:20])
-    # print("тест: \n", test_dataset[10:20])
-
-    # for inputs, targets in test_dataset:
+    # # print("трейн: \n", train_dataset[10:20])
+    # # print("тест: \n", test_dataset[10:20])
+    #
+    # train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    # test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    #
+    # # print("тест: \n", test_dataset[2].values())
+    # # for inputs, targets in test_dataset:
