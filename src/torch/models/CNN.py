704a19d94d30d13032046872aff0aa53cd93cc0c
Daniil Dits <daniil.dits@gmail.com>
2024-03-10 13:20:21 +0300

    gitignore

--- src/torch/models/CNN.py
+++ src/torch/models/CNN.py
@@ -11,6 +11,8 @@ import os
 import math
 from math import exp
 import numpy as np
+import sympy as sp
+
 
 # Гиперпараметры
 # input_size = 2  # Размерность входных данных
@@ -33,30 +35,84 @@ def activation_ln(x):
     return -1.0*math.log(1.0 - (x))
 
 
-class SingleInvNet4(nn.Module):
-    def __init__(self, input_size, idi, device):
+class SingleInvNet2(nn.Module):
+    def __init__(self, input_size, idi, device, l2=0.001):
         """
         y=xA^T+b (b=0)
-        :param input_size:
-        :param idi:
-        :param L2:
+        :param input_size: input data size
+        :param idi: index of neuron
+        :param l2: L2 regularization coefficient
         """
         super().__init__()
+
+        self.l2 = l2
+        self.idi = idi
+
+        self.w11 = nn.Linear(input_size, 1, bias=False).to(device)
+        self.w21 = nn.Linear(input_size, 1, bias=False).to(device)
+
+        self.activation_Exp = activation_Exp
+
+    def forward(self, i: torch.Tensor) -> torch.Tensor:
+        i_ref = i - 3.0
+
+        w11_out = self.w11(i_ref)
+
+        i_sqr = torch.mul(i_ref, i_ref)
+
+        w21_out = self.w21(i_sqr)
+
+        out = torch.cat((w11_out, w21_out))
+        return out
+
+
+
+
+class SingleInvNet4(nn.Module):
+    def __init__(self, input_size, idi, device, l2=0.001):
+        """
+        y=xA^T+b (b=0)
+        :param input_size: input data size
+        :param idi: index of neuron
+        :param l2: L2 regularization coefficient
+        """
+        super().__init__()
+        # self.dropout1 = nn.Dropout(0.1)
+        # self.dropout2 = nn.Dropout(0.5)
+        # self.dropout3 = nn.Dropout(0.1)
+        # self.dropout4 = nn.Dropout(0.5)
+
+        self.l2 = l2
+        self.idi = idi
+
         self.w11 = nn.Linear(input_size, 1, bias=False).to(device)
         self.w21 = nn.Linear(input_size, 1, bias=False).to(device)
         self.w31 = nn.Linear(input_size, 1, bias=False).to(device)
         self.w41 = nn.Linear(input_size, 1, bias=False).to(device)
+
         self.activation_Exp = activation_Exp
-        # self.L2 = L2
-        self.idi = idi
+
+        # self.w11.weight_regularizer = nn.Parameter(torch.Tensor(1), requires_grad=False)
+        # self.w21.weight_regularizer = nn.Parameter(torch.Tensor(1), requires_grad=False)
+        # self.w31.weight_regularizer = nn.Parameter(torch.Tensor(1), requires_grad=False)
+        # self.w41.weight_regularizer = nn.Parameter(torch.Tensor(1), requires_grad=False)
 
     def forward(self, i: torch.Tensor) -> torch.Tensor:
         i_ref = i - 3.0
+
         w11_out = self.w11(i_ref)
+        # w11_out = self.dropout1(w11_out)
+
         w21_out = self.activation_Exp(self.w21(i_ref))
+        # w21_out = self.dropout2(w21_out)
+
         i_sqr = torch.mul(i_ref, i_ref)
+
         w31_out = self.w31(i_sqr)
+        # w31_out = self.dropout3(w31_out)
+
         w41_out = self.activation_Exp(self.w41(i_sqr))
+        # w41_out = self.dropout4(w41_out)
         # out = torch.cat((w11_out, w21_out, w31_out, w41_out), dim=1)
         out = torch.cat((w11_out, w21_out, w31_out, w41_out))
         return out
@@ -68,9 +124,9 @@ class StrainEnergyCANN(nn.Module):
         self.potential_constants = np.zeros(16)
         self.device = device
         self.batch_size = batch_size
-        self.single_inv_net1 = SingleInvNet4(batch_size, 0, device)
-        self.single_inv_net2 = SingleInvNet4(batch_size, 4, device)
-        self.wx2 = nn.Linear(8, 1, bias=False)
+        self.single_inv_net1 = SingleInvNet2(batch_size, 0, device)
+        self.single_inv_net2 = SingleInvNet2(batch_size, 4, device)
+        self.wx2 = nn.Linear(4, 1, bias=False)
 
     # def forward(self, i1: torch.Tensor, i2: torch.Tensor) -> torch.Tensor:
     def forward(self, invariants:torch.Tensor) -> torch.Tensor:
@@ -87,11 +143,11 @@ class StrainEnergyCANN(nn.Module):
 
             i1_out = self.single_inv_net1(i1.unsqueeze(1).unsqueeze(0))
             i2_out = self.single_inv_net2(i2.unsqueeze(1).unsqueeze(0))
-        out = torch.cat((i1_out, i2_out))
+        psi_model = torch.cat((i1_out, i2_out))
         # out = torch.cat((i1_out, i2_out), dim=1)
         # out = out.view(-1, 8)  # Изменение формы перед применением линейного слоя
-        out = self.wx2(out)
-        return out
+        psi_model = self.wx2(psi_model)
+        return psi_model
 
     def get_potential(self):
         """
@@ -99,26 +155,75 @@ class StrainEnergyCANN(nn.Module):
         :return: [weights of model]
         """
         params = []
-        for weights in self.parameters():
+        for id, weights in enumerate(self.parameters()):
+            # print(f"id = {id}, weight = {weights}")
+            if id == 8:
+                weights = weights.tolist()
+                for weight_last_layer in weights[0]:
+                    # print(weight_last_layer)
+                    params.append(weight_last_layer)
+                break
             weight = weights.detach().to('cpu').numpy().copy()
-            params.append(weight)
+            params.append(weight[0, 0].item())
             # params.append(param.data())
 
+        # psi = w2[0] * w1[0] * (I1 - 3) + w2[1] * (sp.exp(w1[1] * (I1 - 3)) - 1) + w2[2] * w1[2] * (I1 - 3) ** 2 + w2[
+        #     3] * (sp.exp(w1[3] * (I1 - 3) ** 2) - 1) + w2[4] * w1[4] * (I2 - 3) + w2[5] * (
+        #                   sp.exp(w1[5] * (I2 - 3)) - 1) + w2[6] * w1[6] * (I2 - 3) ** 2 + w2[7] * (
+        #                   sp.exp(w1[7] * (I2 - 3) ** 2) - 1)
         return params
 
 
 if __name__ == "__main__":
+
+    I1, I2 = sp.symbols('I1 I2')
+    w1 = sp.symbols('w11:19')  # w1, w2, ..., w8
+    w2 = sp.symbols('w21:29')  # w21, w22, ..., w28
+
     torch.manual_seed(42)
-    model = StrainEnergyCANN(batch_size=1)
+    trained_model = StrainEnergyCANN(batch_size=1, device="cpu")
+    trained_model.load_state_dict(torch.load('../pretrained_models/CNN_l2/20240226_132313_6.pth'))
+
+
     # y = model(torch.tensor([[0.1]]), torch.tensor([[0.1]]))
-    y = model(torch.tensor([[0.1], [0.1]]))
+    # y = model(torch.tensor([[0.1], [0.1]]))
     # x = [torch.tensor([[0.1], [0.1]]), torch.tensor([[0.1], [0.1]])]
     # y = model(x)
-    print(y)
+    # print(y)
 
+
+    psi = (w2[0] * w1[0] * (I1 - 3)
+           + w2[1] * (sp.exp(w1[1] * (I1 - 3)) - 1)
+           + w2[2] * w1[2] * (I1 - 3) ** 2
+           + w2[3] * (sp.exp(w1[3] * (I1 - 3) ** 2) - 1)
+           + w2[4] * w1[4] * (I2 - 3)
+           + w2[5] * (sp.exp(w1[5] * (I2 - 3)) - 1)
+           + w2[6] * w1[6] * (I2 - 3) ** 2
+           + w2[7] * (sp.exp(w1[7] * (I2 - 3) ** 2) - 1))
+
+    coefs = trained_model.get_potential()
+    print(coefs)
+    l2_reg = None
+    for param in coefs:
+        print(param)
+        if l2_reg is None:
+            l2_reg = param**2
+        else:
+            l2_reg = l2_reg + param**2
+        print("l2 reg = ", l2_reg)
+    print(l2_reg)
+
+    w1 = coefs[:8]
+    w2 = coefs[8:]
+    psi = psi.subs([(w1, w2)])
+
+    # print(sp.pretty(psi))
+
+    # Вывод функции ψ(I1, I2)
+    print(psi)
     # Получение графа вычислений
-    dot = make_dot(y, params=dict(model.named_parameters()))
-    dot.render('CNN', format='png')
+    # dot = make_dot(y, params=dict(model.named_parameters()))
+    # dot.render('CNN', format='png')
 
 """
 TODO:
--- src/torch/train_test.py
+++ src/torch/train_test.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -16,19 +18,20 @@ from models.CNN import StrainEnergyCANN
 # print(torch.cuda.device_count())
 # print(torch.cuda.current_device())
 # print(torch.cuda.get_device_name(0))
+
 if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
 print(f"Selected device: {device}")
 device = "cpu"
+
 # Гиперпараметры
 input_size = 2  # Размерность входных данных
 output_size = 1  # Размерность выходных данных
 hidden_size = 270  # Новое количество нейронов на слое
-learning_rate = 0.001
-EPOCHS = 100
-
+learning_rate = 0.1
+EPOCHS = 10
 timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
 
@@ -68,23 +71,24 @@ def load_data(path_to_exp_names, batch_size):
 
 
 def train(train_loader, test_loader, experiment_name, plot_loss=False):
-
     batch_size = train_loader.batch_size
     # Инициализация модели, функции потерь и оптимизатора
     model = StrainEnergyCANN(batch_size, device=device).to(device)
+
+
     loss_fn = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.001)
 
-    def train_one_epoch(epoch_index, tb_writer):
+    def train_one_epoch(epoch_index, tb_writer, l2_reg_coeff=0.001):
 
-        losses = []
         running_loss = 0.
         last_loss = 0.
 
         for i, data in enumerate(train_loader):
+
+            last_data = len(train_loader)
             inputs, targets = data
             inputs, targets = inputs.to(device), targets.to(device)
-
             # if inputs.shape != (batch_size, 2):
             #     it += 1
             #     print(it)
@@ -93,22 +97,44 @@ def train(train_loader, test_loader, experiment_name, plot_loss=False):
             # i1_inputs=inputs[:, :1]
             # i2_inputs = inputs[:, 1:]
             outputs = model(inputs)
+
             loss = loss_fn(outputs, targets)
+
+            # coefs = model.get_potential()
+
+            # l2_reg = None
+            # for param in coefs:
+            #     # print(param)
+            #     if l2_reg is None:
+            #         l2_reg = param ** 2
+            #     else:
+            #         l2_reg = l2_reg + param ** 2
+            #     # print("l2 reg = ", l2_reg)
+            #
+            # # Добавляем L2 регуляризацию к функции потерь
+            # if l2_reg is not None:
+            #     loss = loss + l2_reg_coeff * l2_reg
+
             loss.backward()
             optimizer.step()
 
+            # print('experiment {} loss: {}'.format(i + 1, loss.item()))
+            tb_x = epoch_index * len(train_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', loss.item(), tb_x)
+            last_loss = loss.item()
             running_loss += loss.item()
-            if i % 10000 == 999:
-                last_loss = running_loss / 1000  # loss per batch
-                print('  batch {} loss: {}'.format(i + 1, last_loss))
-                tb_x = 1 * len(train_loader) + i + 1
-                tb_writer.add_scalar('Loss/train', last_loss, tb_x)
-                running_loss = 0.
-
-        return last_loss
+            # if i % last_data == last_data - 1:
+            #     last_loss = running_loss / last_data  # loss per experiment
+            #     print('experiment {} loss: {}'.format(i + 1, last_loss))
+            #     tb_x = epoch_index * len(train_loader) + i + 1
+            #     tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            #     running_loss = 0.
+            # if i == (last_data-1):
+            #     print(last_data)
+        return running_loss / last_data
 
     epoch_number = 0
-    best_vloss = 1_000_000.
+    best_vloss = torch.inf
     losses = []
 
     # Обучение модели
@@ -128,7 +154,7 @@ def train(train_loader, test_loader, experiment_name, plot_loss=False):
         with torch.no_grad():
             for i, vdata in enumerate(test_loader):
                 vinputs, vlabels = vdata
-                voutputs = model(vinputs)
+                voutputs = model(vinputs).to(device)
                 vloss = loss_fn(voutputs, vlabels)
                 running_vloss += vloss
 
@@ -145,19 +171,22 @@ def train(train_loader, test_loader, experiment_name, plot_loss=False):
         # Track best performance, and save the model's state
         if avg_vloss < best_vloss:
             best_vloss = avg_vloss
-            model_path = 'model_{}_{}'.format(timestamp, epoch_number)
-            torch.save(model.state_dict(), model_path)
+        # torch.save(model.state_dict(), model_path)
 
-        losses.append(best_vloss)
+        model_path = '{}_{}'.format(timestamp, epoch_number)
+        path_to_save_weights = os.path.join("pretrained_models", experiment_name)
+        path_to_save_weights = os.path.join(path_to_save_weights, model_path + ".pth")
+        torch.save(model.state_dict(), path_to_save_weights)
+        print(f"Saved PyTorch Model State to {experiment_name}.pth")
+
+        losses.append(avg_vloss)
         epoch_number += 1
-
+        plt.plot(math.log(avg_vloss))
+        plt.show()
         # if (epoch + 1) % 10 == 0:
         #     print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}')
 
-    if experiment_name is not None:
-        path_to_save_weights = os.path.join("pretrained_models", experiment_name + ".pth")
-        torch.save(model.state_dict(), path_to_save_weights)
-        print(f"Saved PyTorch Model State to {experiment_name}.pth")
+    # if experiment_name is not None:
 
     if plot_loss:
         plt.plot(losses)
@@ -169,8 +198,6 @@ def train(train_loader, test_loader, experiment_name, plot_loss=False):
 
 
 def test(model, dataloader, plot_err=False):
-    batch_size = dataloader.batch_size
-
     model.eval()
     all_predictions = []
     all_targets = []
@@ -211,10 +238,10 @@ def test(model, dataloader, plot_err=False):
             plt.show()
             plt.savefig("Errs")
         except:
-             print("can't plot")
+            print("can't plot")
 
 
-def jit(model,  experiment_name, x=None):
+def jit(model, experiment_name, x=None):
     # traced_net = torch.jit.trace(model, x)
     traced_net = torch.jit.script(model)
     directory = "pretrained_models/"
@@ -228,37 +255,30 @@ def jit(model,  experiment_name, x=None):
 if __name__ == "__main__":
     torch.manual_seed(42)
 
-    experiment = "CNN3"
+    experiment = "CNN_l2"
 
     print("loading data...")
     # train_dataloader, test_dataloader = load_data("full_data_names.txt", batch_size=1)
-    train_dataloader = load_data("without_one.txt", batch_size=1)
-    test_dataloader = load_data("one_data_names.txt", batch_size=1)
-
+    train_dataloader = load_data("one_data_names.txt", batch_size=1)
+    test_dataloader = load_data("another_one_data_name.txt", batch_size=1)
 
     trained_model = train(
-                        train_dataloader,
-                        test_dataloader,
-                        plot_loss=True,
-                        experiment_name=experiment)
+        train_dataloader,
+        test_dataloader,
+        plot_loss=True,
+        experiment_name=experiment)
 
     print("test data...")
     # trained_model = StrainEnergyCANN(batch_size=1, device=device)
-    # trained_model.load_state_dict(torch.load('pretrained_models/experimental.pth'))
-    # test(trained_model, test_dataloader, plot_err=True)
+    # trained_model.load_state_dict(torch.load('pretrained_models\CNN3_20240204_222321_13.pth'))
+    test(trained_model, test_dataloader, plot_err=True)
 
-    weights = trained_model.get_potential()
-    print(weights)
-    # for param in trained_model.parameters():
-    #     print(param, type(param), param.size())
-
-    
 
 
     # x1 = torch.randn(1).to(device)
     # x2 = torch.randn(1).to(device)
     # # print(x1)
-    jit(trained_model,  experiment_name=experiment)
+    jit(trained_model, experiment_name=experiment)
 
 """
 TODO:
