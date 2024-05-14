f108309609acb011d52b66d99aaef7e727c04fb1
23Grammer58 <daniil.dits@gmail.com>
2024-05-14 19:29:45 +0300

    test units

--- src/CANN_torch/train_unit.py
+++ src/CANN_torch/train_unit.py
@@ -1,238 +1,122 @@
+
+
+from models.CNN import StrainEnergyCANN_C
+
+from trainer import Trainer
+import argparse
+import logging
+import sys
+import tempfile
+from argparse import Namespace
+from typing import List, Tuple, get_args
+
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-from torch.utils.tensorboard import SummaryWriter
-from datetime import datetime
-import os
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-from typing import Optional
-
-from models.CNN import StrainEnergyCANN, StrainEnergyCANN_C
-from utils.dataload import ExcelDataset, normalize_data
-from utils.visualisation import *
+from torcheval.metrics import R2Score
+from torchtnt.framework.state import State
+from torchtnt.framework.train import train
+from torchtnt.framework.unit import TrainUnit
+from torchtnt.utils import copy_data_to_device, init_from_env, seed, TLRScheduler
+from torchtnt.utils.loggers import TensorBoardLogger
+from torchnet.logger import VisdomLogger
 
 
-class Trainer:
-    def __init__(self,
-                 experiment_name: Optional[str] = "test",
-                 model: Optional[nn.Module] = StrainEnergyCANN_C,
-                 device: Optional[str] = "cpu",
-                 learning_rate: Optional[float] = 0.001,
-                 epochs: Optional[int] = 100,
-                 plot_valid: Optional[bool] = False,
-                 batch_size: Optional[int] = 1,
-                 l1_reg_coeff: Optional[float] = 0.001,
-                 l2_reg_coeff: Optional[float] = 0.001
-                 ):
-        """
-        A class for training CANN model.
+_logger: logging.Logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+Batch = Tuple[torch.tensor, torch.tensor]
+# specify type of the data in each batch of the dataloader to allow for typechecking
 
-        Attributes:
-            experiment_name (str): Name of the experiment. Defaults to "test".
-            model (nn.Module): The neural network model to be trained. Defaults to `StrainEnergyCANN_C`.
-            device (str): The device to run the training on. Can be "cpu" or "cuda". Defaults to "cpu".
-            learning_rate (float): The learning rate for the optimizer. Defaults to 0.1.
-            epochs (int): The number of epochs to train for. Defaults to 100.
-            plot_valid (bool): Whether to plot the validation loss. Defaults to False.
-            batch_size (int): The batch size for training. Defaults to 1.
-            timestamp (str): The timestamp of the training session.
-            writer (SummaryWriter): The TensorBoard writer for logging.
-        """
-
-        self.l1_reg_coeff = l1_reg_coeff
-        self.l2_reg_coeff = l2_reg_coeff
-        self.model = model(batch_size, device=device)
+class CANNTrainUnit(TrainUnit[Batch]):
+    def __init__(
+        self,
+        module: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        lr_scheduler: torch.optim.lr_scheduler._LRScheduler,
+        device: torch.device,
+        train_accuracy: R2Score,
+        tb_logger: TensorBoardLogger,
+        log_every_n_steps: int
+    ):
+        super().__init__()
+        self.module = module
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
         self.device = device
-        self.learning_rate = learning_rate
-        self.epochs = epochs
-        self.experiment_name = experiment_name
-        self.plot_valid = plot_valid
-        self.timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-        self.writer = SummaryWriter('runs/fashion_trainer_{}'.format(self.timestamp))
-        self.path_to_save_weights = os.path.join("pretrained_models", self.experiment_name)
-        self.batch_size = batch_size
-        self.path_to_best_weights = None
 
-    def load_data(self,
-                  path_to_exp_names,
-                  transform=normalize_data,
-                  shuffle=True,
-                  length_start=None,
-                  length_end=None):
+        # create an accuracy Metric to compute the accuracy of training
+        self.train_accuracy = train_accuracy
+        self.log_every_n_steps = log_every_n_steps
 
-        dataset = ExcelDataset(
-                               path=path_to_exp_names,
-                               transform=transform,
-                               device=self.device,
-                               batch_size=self.batch_size)
+        self.tb_logger = tb_logger
 
-        dataset.to_tensor()
-        if length_end is not None:
-            dataset.data = dataset.data[length_start:length_end]
+    def train_step(self, state: State, data: Batch) -> None:
+        data = copy_data_to_device(data, self.device)
 
-        dataset_loader = DataLoader(
-                                    dataset,
-                                    batch_size=self.batch_size,
-                                    shuffle=shuffle,
-                                    num_workers=1,
-                                    pin_memory=False)
-        return dataset_loader
+        inputs, targets = data
+        outputs = self.module(inputs)
+        loss_fn = torch.nn.MSELoss()
+        loss = loss_fn(outputs, targets) * inputs[-1]
+        loss.backward()
 
-    def train(self, train_loader, test_loader):
-        # Initialize the model, loss function, and optimizer
+        self.optimizer.step()
+        self.optimizer.zero_grad()
 
-        if self.experiment_name is not None:
-            path_to_save_weights = os.path.join("pretrained_models", self.experiment_name)
-            if not os.path.exists(path_to_save_weights):
-                os.makedirs(path_to_save_weights)
-                print(f"Directory {path_to_save_weights} created successfully")
-            else:
-                print(f"Directory {path_to_save_weights} already exists")
+        self.train_accuracy.update(outputs, targets)
+        step_count = self.train_progress.num_steps_completed
+        if (step_count + 1) % self.log_every_n_steps == 0:
+            acc = self.train_accuracy.compute()
+            self.tb_logger.log("loss", loss, step_count)
+            self.tb_logger.log("R2", acc, step_count)
 
-        loss_fn = nn.MSELoss()
-        optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+    def on_train_epoch_end(self, state: State) -> None:
+        # compute and log the metric at the end of the epoch
+        step_count = self.train_progress.num_steps_completed
+        acc = self.train_accuracy.compute()
+        self.tb_logger.log("accuracy_epoch", acc, step_count)
 
-        last_data = len(train_loader)
-        def train_one_epoch(epoch_index):
-            running_loss = 0.
-            last_loss = 0.
+        # reset the metric at the end of every epoch
+        self.train_accuracy.reset()
 
-            for i, data in enumerate(train_loader):
-                features, target = data
-                _, _, _, _, exp_type = features
-                # inputs = (F, i1, i2, exp_type)
-
-                optimizer.zero_grad()
-                stress_model = self.model(features)
-
-                loss = loss_fn(stress_model, target) * exp_type
-
-                # print(loss.item())
-                # l1_reg = self.model.calc_l1()
-                # l2_reg = self.model.calc_regularization(2)
-                # if l2_reg is not None:
-                #     loss = loss + 0.5 * self.l2_reg_coeff * l2_reg + self.l1_reg_coeff * l1_reg
-
-                loss.backward(retain_graph=True)
-                # loss.backward()
-                optimizer.step()
-
-                tb_x = epoch_index * len(train_loader) + i + 1
-                self.writer.add_scalar('Loss/train', loss.item(), tb_x)
-                # last_loss = loss.item()
-                running_loss += loss.item()
-
-            return running_loss
-
-        epoch_number = 0
-        best_vloss = torch.inf
-        elosses = []
-        vlosses = []
-        vpredictions = []
-        vtargets = []
-
-        # Training the model
-        for epoch in tqdm(range(self.epochs)):
-            self.model.train(True)
-            avg_loss = train_one_epoch(epoch_number)
-
-            running_vloss = 0.0
-            self.model.eval()
-
-            validation = False
-            if validation:
-                with torch.no_grad():
-                    for i, vdata in enumerate(test_loader):
-                        vfeatures, vtarget = vdata
-
-                        optimizer.zero_grad()
-                        # stress_model = self.model(vfeatures)
-                        vstress = self.model(vfeatures)
-                        vloss = loss_fn(vtarget, vstress)
-                        running_vloss += vloss
-
-                        # targets_P11.append(vtarget[0, 0])
-                        # targets_P12.append(vtarget[0, 1])
-                        # predictions_P11.append(vstress[0, 0])
-                        # predictions_P12.append(vstress[0, 1])
-
-                        vlosses.append(vloss)
-                        vpredictions.append(vstress)
-                        vtargets.append(vtarget)
-            else:
-                running_vloss = avg_loss
-            avg_vloss = running_vloss / last_data
-            print()
-            # print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
-
-            print(f'Epoch [{epoch + 1}/{self.epochs}], Loss: {avg_loss:.4f}')
-            if avg_loss < best_vloss:
-                best_vloss = avg_loss
-                model_path = '{}_{}'.format(self.timestamp, epoch)
-                path_to_save_weights = os.path.join(self.path_to_save_weights, model_path + ".pth")
-                print(f"Saved PyTorch Model State to {path_to_save_weights}")
-                torch.save(self.model.state_dict(), path_to_save_weights)
-                self.path_to_best_weights = path_to_save_weights
-
-                # print(self.model.get_potential())
-
-            elif epoch % 10 == 0:
-                # print(f'Epoch [{epoch + 1}/{self.epochs}], Loss: {avg_loss:.4f}')
-                model_path = '{}_{}'.format(self.timestamp, epoch)
-                path_to_save_weights = os.path.join(self.path_to_save_weights, model_path + ".pth")
-                print(f"Saved PyTorch Model State to {path_to_save_weights}")
-                torch.save(self.model.state_dict(), path_to_save_weights)
-            elosses.append(avg_loss)
-            # epoch_number += 1
-            print(self.model.get_potential())
-
-        plt.plot(elosses)
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.title('Training Loss')
-        plt.show()
-        if self.plot_valid:
-            plt.figure(figsize=(10, 5))
-            plt.plot(vpredictions, label='Ppred', color='red')
-            plt.plot(vtargets, label='P_true', color='black')
-            plt.xlabel('lambda/gamma')
-            plt.ylabel('P')
-            plt.title('Predictions vs. Targets')
-            plt.legend()
-            plt.show()
-
-        return self
+        # step the learning rate scheduler
+        self.lr_scheduler.step()
 
 
-def main():
+
+def main(argv: List[str]) -> None:
+    args = get_args(argv)
+
+    device = "cpu"
+    path = tempfile.mkdtemp()
+    tb_logger = TensorBoardLogger(path)
+
     data_path = r"C:\Users\drani\dd\data-driven-constitutive-modelling\data\brain_bade\CANNsBRAINdata.xlsx"
 
     test_train = Trainer(plot_valid=True, epochs=6000, experiment_name="FIRST_weights", l2_reg_coeff=0.001)
-    # train_data_loader = test_train.load_data(data_path, transform=None)
-    test_data_loader = test_train.load_data(data_path, transform=None, shuffle=False)
-    # trained_model = test_train.train(train_data_loader, test_data_loader)
-    trained_model = StrainEnergyCANN_C()
-    trained_model.load_state_dict(torch.load(
-        r"/src/CANN_torch\pretrained_models\FIRST_weights\20240502_153939_734.pth"))
-    trained_model.eval()
-    vpredictions = []
-    vtargets = []
-    for data in test_data_loader:
-        features, target = data
-        if features[-1] == 1.5:
-            vpredictions.append(trained_model(features).detach().numpy())
-            vtargets.append(target.detach().numpy())
+    train_dataloader = test_train.load_data(data_path, transform=None)
+    # test_data_loader = test_train.load_data(data_path, transform=None, shuffle=False)
 
-    plt.figure(figsize=(10, 5))
-    plt.plot(vpredictions, label='P_pred', color='red')
-    plt.plot(vtargets, label='P_true', color='black')
-    plt.xlabel('lambda/gamma')
-    plt.ylabel('P')
-    plt.title('Predictions vs. Targets')
-    plt.legend()
-    plt.show()
+    # trained_model = test_train.train(train_data_loader, test_data_loader)
+    model = StrainEnergyCANN_C(1, device="cpu")
+    optimizer = torch.optim.Adam(model.parameters())
+    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.9)
+    train_accuracy = R2Score(device=device)
+
+    my_unit = CANNTrainUnit(
+        model,
+        optimizer,
+        lr_scheduler,
+        device,
+        train_accuracy,
+        tb_logger,
+        2,
+    )
+
+    train(
+        my_unit,
+        train_dataloader=train_dataloader,
+        max_epochs=10,
+    )
+
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
